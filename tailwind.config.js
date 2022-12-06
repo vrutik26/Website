e68b8f4343368c0dec696e571479c8e3069a428b
vrutik26 <vikiprajapati121314@gmail.com>
2022-12-06 19:51:56 +0530

    config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,26 +1,29 @@
 /** @type {import('tailwindcss').Config} */
-export const content = [
-  "./pages/**/*.{js,ts,jsx,tsx}",
-  "./components/**/*.{js,ts,jsx,tsx}",
-];
-export const theme = {
-  extend: {
-    keyframes: {
-      'fade-in-down': {
-        '0%': {
-          opacity: '0',
-          transform: 'translateY(-10px)'
-        },
-        '100%': {
-          opacity: '1',
-          transform: 'translateY(0)'
-        },
-      }
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    
+  ],
+  theme: {
+    extend: {
+      //  keyframes: {
+      //           'fade-in-down': {
+      //               '0%': {
+      //                   opacity: '0',
+      //                   transform: 'translateY(-10px)'
+      //               },
+      //               '100%': {
+      //                   opacity: '1',
+      //                   transform: 'translateY(0)'
+      //               },
+      //           }
+      //       },
+      //       animation: {
+      //           'fade-in-down': 'fade-in-down 4s ease-out'
+      //       }
     },
-    animation: {
-      'fade-in-down': 'fade-in-down 4s ease-out'
-    }
   },
-};
-export const plugins = [];
+  plugins: [],
+}
 
